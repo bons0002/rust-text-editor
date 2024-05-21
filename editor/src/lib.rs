8c56093e11bac9286d7cc85e2eab9f40acda22e4
bons0002 <sam_bons@yahoo.com>
2024-05-21 12:03:18 -0500

    Add basic input handling
    Currently, characters, uppercase characters, and newline characters can be entered into the editor block.

--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
+
+// Get the key pressed
+pub fn handle_input(text: &mut String) {
+    match event::read().unwrap() {
+        // Return the character if only a key (without moodifier key) is pressed
+        Event::Key(KeyEvent {
+            code,
+            modifiers: KeyModifiers::NONE,
+            ..
+        }) => {
+            // Return the key
+            match code {
+                KeyCode::Char(code) => text.push(code),
+                KeyCode::Enter => text.push('\n'),
+                _ => (),
+            }
+        },
+
+        // Uppercase letters (Using shift)
+        Event::Key(KeyEvent {
+            code,
+            modifiers: KeyModifiers::SHIFT,
+            ..
+        }) => {
+            match code {
+                KeyCode::Char(code) => text.push(code.to_ascii_uppercase()),
+                _ => ()
+            }
+        }
+
+        _ => (),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
+
