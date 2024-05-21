b8e0baa8a44891b77d319716b6a24a4efc4ae450
bons0002 <sam_bons@yahoo.com>
2024-05-21 10:56:14 -0500

    Create a (very) temp ui
    This creates a very temporary ui framework to work within. In future, ui will be rearranged, be more functional, and will adapt to window resizing.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -15,7 +15,8 @@ use ratatui::{
 use std::thread;
 use std::time::Duration;
 
-pub fn run() -> io::Result<()> {
+// Main driver function
+pub fn run(filename: &str) -> io::Result<()> {
     // Put stdout into raw mode (turn off canonical mode)
     enable_raw_mode()?;
     // Switches the terminal to an alternate screen
@@ -25,7 +26,7 @@ pub fn run() -> io::Result<()> {
 
     // Draw the terminal widgets
     // Temporarily not handling errors
-    let _ = draw_terminal();
+    let _ = draw_terminal(filename);
 
     // Turn off raw mode for stdout (enable canonical mode)
     disable_raw_mode()?;
@@ -35,13 +36,14 @@ pub fn run() -> io::Result<()> {
     Ok(())
 }
 
-fn draw_terminal() -> io::Result<()> {
+// Create the terminal and draw the ui
+fn draw_terminal(filename: &str) -> io::Result<()> {
     // Create a new terminal
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
     loop {
         terminal.draw(|frame| {
-            ui(frame);
+            ui(frame, filename);
         })?;
         thread::sleep(Duration::from_secs(5));
         break;
@@ -50,9 +52,38 @@ fn draw_terminal() -> io::Result<()> {
     Ok(())
 }
 
-fn ui(frame: &mut Frame) {
+// Define the frame ui
+fn ui(frame: &mut Frame, _filename: &str) {
+    // Create tabs (TEMP)
+    let tabs_layout = Layout::new(
+        Direction::Vertical,
+        [Constraint::Percentage(5), Constraint::Percentage(95)],
+    )
+    .split(frame.size());
     frame.render_widget(
-        Block::bordered(),
-        frame.size()
+        Tabs::new(vec!["Tab1", "Tab2", "Tab3", "Tab4"])
+            .block(Block::bordered())
+            .style(Style::default().white())
+            .highlight_style(Style::default().green())
+            .select(2)
+            .divider(symbols::DOT)
+            .padding(" ", " "),
+    tabs_layout[0]
+    );
+    // Create the rest of the frame
+    let main_layout = Layout::new(
+        Direction::Horizontal,
+        [Constraint::Percentage(10), Constraint::Percentage(90)],
+    )
+    .split(tabs_layout[1]);
+    // File explorer
+    frame.render_widget(
+        Block::new().title("Explorer").borders(Borders::ALL),
+        main_layout[0],
+    );
+    // Main editor space
+    frame.render_widget(
+        Block::new().borders(Borders::ALL),
+        main_layout[1],
     );
 }
