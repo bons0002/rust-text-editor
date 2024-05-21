175590003b044a8d681a316c01e1b9fbef49aa05
bons0002 <sam_bons@yahoo.com>
2024-05-21 10:55:14 -0500

    Add filename as args
    This adds a temp way to pass a filename as a commandline argument. In the future, filenames will be provided once the app is running, not as cli args. Also, multiple files will be able to be openned at once.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,14 @@
-use ratatui::{prelude::*, widgets::*};
-
 use app::run;
 
+use std::env;
+
 fn main() {
-    match run() {
+    // Get the name of the file to be openned from the cli args
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    // Run the app
+    match run(filename) {
         Ok(_) => (),
         _ => panic!("Unresolved issue"),
     };
