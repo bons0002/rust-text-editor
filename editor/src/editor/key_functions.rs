ea2eabdb8602d9680d0cd72eb99f9d49ce0f44c1
bons0002 <sam_bons@yahoo.com>
2024-06-01 12:13:37 -0500

    Add visual selection highlighting
    The highlighted selection of text now visually highlights. There are also now some formatting changes, and the end key now goes to the end of the visual line rather than the full line.

--- editor/src/editor/key_functions.rs
+++ editor/src/editor/key_functions.rs
@@ -1,7 +1,7 @@
 // Implementation of the module `key_functions` defined in `src/lib.rs` module `editor`
 
+use super::{Config, EditorSpace, File};
 use std::io::Write;
-use super::{EditorSpace, File, Config};
 
 mod cursor_line;
 
@@ -19,7 +19,7 @@ pub fn enter_key(editor: &mut EditorSpace) {
 	// Get the current cursor position
 	let loc = (editor.pos.0, editor.pos.1);
 	let line = &editor.content[loc.1];
-	
+
 	// Get the rest of the line after the cursor
 	let after_cursor = get_after_cursor(line, loc.0);
 
@@ -36,25 +36,24 @@ pub fn enter_key(editor: &mut EditorSpace) {
 // Functionality of the backspace key
 pub fn backspace(editor: &mut EditorSpace, config: &Config) {
 	let line = editor.content[editor.pos.1].clone();
-		// Remove empty line
-		if editor.pos.0 <= 1 {  // If cursor at beginning of line, move to above line
-			if editor.content.len() > 1 {
-				// Get the text from the rest of the line after the cursor
-				let after_cursor = get_after_cursor(&line, editor.pos.0);
-				// Remove the current line
-				editor.content.remove(editor.pos.1);
-				// Move up one line
-				up_arrow(editor, config);
-				end_key(editor, config);
-				// Append the rest of the line to the previous line (where the cursor is moving to)
-				editor.content[editor.pos.1].push_str(after_cursor);
-			}
-		} else {
-			// Move cursor left
-			left_arrow(editor, config);
-			// Remove one character
-			editor.content[editor.pos.1].remove(editor.pos.0 - 1);
+	// Remove empty line
+	if editor.pos.0 <= 1 {  // If cursor at beginning of line, move to above line
+		if editor.content.len() > 1 {
+			// Get the text from the rest of the line after the cursor
+			let after_cursor = get_after_cursor(&line, editor.pos.0);
+			// Remove the current line
+			editor.content.remove(editor.pos.1);
+			// Move up one line
+			up_arrow(editor, config);
+			end_key(editor, config);
+			// Append the rest of the line to the previous line (where the cursor is moving to)
+			editor.content[editor.pos.1].push_str(after_cursor);
 		}
+	} else {    // Move cursor left
+		left_arrow(editor, config);
+		// Remove one character
+		editor.content[editor.pos.1].remove(editor.pos.0 - 1);
+	}
 }
 
 // Functionality of the delete key
@@ -75,7 +74,7 @@ pub fn delete_key(editor: &mut EditorSpace) {
 
 // Get the rest of the text on the line after the cursor
 fn get_after_cursor(line: &String, loc: usize) -> &str {
-	// Get the rest of the line and store 
+	// Get the rest of the line and store
 	&line[loc - 1..]
 }
 
@@ -100,7 +99,7 @@ pub fn left_arrow(editor: &mut EditorSpace, config: &Config) {
 			editor.pos = (editor.pos.0 - 1, editor.pos.1);
 			editor.cursor_pos = (editor.cursor_pos.0 - config.tab_width, editor.cursor_pos.1);
 		}
-	} else { // Otherwise
+	} else {
 		home_key(editor);
 	}
 }
@@ -117,7 +116,7 @@ pub fn right_arrow(editor: &mut EditorSpace, config: &Config) {
 			editor.pos = (editor.pos.0 + 1, editor.pos.1);
 			editor.cursor_pos = (editor.cursor_pos.0 + config.tab_width, editor.cursor_pos.1);
 		}
-	} else { // Otherwise
+	} else {
 		end_key(editor, config);
 	}
 }
@@ -127,25 +126,13 @@ pub fn up_arrow(editor: &mut EditorSpace, config: &Config) {
 	// Ensure that the cursor doesn't move above the editor block
 	if editor.cursor_pos.1 > editor.height.0 + 1 {
 		// Move the cursor to the previous line
-		cursor_line::move_cursor_line(
-			editor,
-			config,
-			cursor_line::Operation::SUB,
-			1,
-			1,
-		);
+		cursor_line::move_cursor_line(editor, config, cursor_line::Operation::SUB, 1, 1);
 	} else if editor.scroll_offset.0 > 0 {	// If the cursor moves beyond the bound, scroll up
 		// Scroll up
 		editor.scroll_offset = (editor.scroll_offset.0 - 1, editor.scroll_offset.1);
-		
+
 		// Move to the previous line in the text, but don't move the screen cursor
-		cursor_line::move_cursor_line(
-			editor,
-			config,
-			cursor_line::Operation::SUB,
-			1,
-			0,
-		);
+		cursor_line::move_cursor_line(editor, config, cursor_line::Operation::SUB, 1, 0);
 	}
 }
 
@@ -156,25 +143,14 @@ pub fn down_arrow(editor: &mut EditorSpace, config: &Config) {
 		// Ensure that the cursor doesn't move below the editor block
 		if editor.cursor_pos.1 < ((editor.height.1 - editor.height.0) + 1) {
 			// Move the cursor to the next line
-			cursor_line::move_cursor_line(
-				editor,
-				config,
-				cursor_line::Operation::ADD,
-				1,
-				1,
-			);
-		} else if editor.scroll_offset.0 < editor.content.len() as u16 {  // If the cursor goes below the bound, scroll down
+			cursor_line::move_cursor_line(editor, config, cursor_line::Operation::ADD, 1, 1);
+		} else if editor.scroll_offset.0 < editor.content.len() as u16 {
+			// If the cursor goes below the bound, scroll down
 			// Scroll down
 			editor.scroll_offset = (editor.scroll_offset.0 + 1, editor.scroll_offset.1);
-			
+
 			// Move the position in the text, but don't move the screen cursor
-			cursor_line::move_cursor_line(
-				editor,
-				config,
-				cursor_line::Operation::ADD,
-				1,
-				0,
-			);
+			cursor_line::move_cursor_line(editor, config, cursor_line::Operation::ADD, 1, 0);
 		}
 	}
 }
@@ -209,9 +185,20 @@ pub fn end_key(editor: &mut EditorSpace, config: &Config) {
 	// Count the number of tab characters
 	let tab_chars = editor.content[editor.pos.1].matches('\t').count() * (config.tab_width - 1);
 
-	// Move to end of line
-	editor.pos = (editor.content[editor.pos.1].len() + 1, editor.pos.1);
-	editor.cursor_pos = (editor.width.0 + editor.content[editor.pos.1].len() + 1 + tab_chars, editor.cursor_pos.1);
+	// Move to end of line if not past the end of the block
+	if editor.content[editor.pos.1].len() < (editor.width.1 - editor.width.0) {
+		editor.pos = (editor.content[editor.pos.1].len() + 1, editor.pos.1);
+		editor.cursor_pos = (
+			editor.width.0 + editor.content[editor.pos.1].len() + 1 + tab_chars,
+			editor.cursor_pos.1,
+		);
+	} else {	// If line longer than width of block, move to the end of the 'visible' line
+		editor.pos = ((editor.width.1 - editor.width.0) - 1, editor.pos.1);
+		editor.cursor_pos = (
+			editor.width.0 + (editor.width.1 - editor.width.0) + tab_chars - 1,
+			editor.cursor_pos.1,
+		);
+	}
 }
 
 // Save key combo functionality
@@ -223,20 +210,20 @@ pub fn save_key_combo(editor: &mut EditorSpace) {
 	};
 	// Write the content to the new file
 	for line in &editor.content {
-		write!(file, "{}\n", line).unwrap();
+		writeln!(file, "{}", line).unwrap();
 	}
 }
 
 pub fn highlight_right(editor: &mut EditorSpace, config: &Config) {
 	// If there is currently no selection
-	if editor.selection == ((-1,-1), (-1,-1)) {
+	if editor.selection == ((-1, -1), (-1, -1)) {
 		// Set the starting selection point
 		let begin = (editor.pos.0 as isize - 1, editor.pos.1 as isize);
 		editor.selection = (begin, begin);
 		// Move right
 		right_arrow(editor, config);
 		// Set endpoint for selection
-		let end = (editor.pos.0 as isize, editor.pos.1 as isize);
+		let end = (editor.pos.0 as isize - 1, editor.pos.1 as isize);
 		editor.selection = (editor.selection.0, end);
 	} else {
 		// Move right
@@ -249,9 +236,9 @@ pub fn highlight_right(editor: &mut EditorSpace, config: &Config) {
 
 #[cfg(test)]
 mod tests {
+	use crate::editor::key_functions;
 	use crate::editor::EditorSpace;
 	use config::config::Config;
-	use crate::editor::key_functions;
 
 	// Test the highlight right function
 	#[test]
@@ -262,19 +249,20 @@ mod tests {
 		let mut editor = EditorSpace::new(filename, &config);
 
 		// Set starting pos in text
-		editor.pos = (1,0);
+		editor.pos = (1, 0);
 
 		// Highlight 3 characters (123)
 		for _i in 0..3 {
 			key_functions::highlight_right(&mut editor, &config);
 		}
-		
+
 		// Check that the endpoints were updated correctly
-		assert_eq!(editor.selection, ((0,0), (3,0)));
-		assert_ne!(editor.selection, ((1,0), (4,0)));
+		assert_eq!(editor.selection, ((0, 0), (3, 0)));
+		assert_ne!(editor.selection, ((1, 0), (4, 0)));
 
 		// Check that the content of the highlighted section is correct
-		let selected_string = &editor.content[editor.pos.1][(editor.selection.0).0 as usize..(editor.selection.1).0 as usize];
+		let selected_string = &editor.content[editor.pos.1]
+			[(editor.selection.0).0 as usize..(editor.selection.1).0 as usize];
 		assert_eq!(selected_string, "123");
 	}
 
@@ -286,13 +274,13 @@ mod tests {
 		let mut editor = EditorSpace::new(filename, &config);
 
 		// Set starting pos in text
-		editor.pos = (1,0);
+		editor.pos = (1, 0);
 		// Highlight 3 character
 		for _i in 0..3 {
 			key_functions::highlight_right(&mut editor, &config);
 		}
 		// Reset selection
-		editor.selection = ((-1,-1), (-1,-1));
+		editor.selection = ((-1, -1), (-1, -1));
 
 		// Select next 3 (456)
 		for _i in 0..3 {
@@ -300,12 +288,14 @@ mod tests {
 		}
 
 		// Check endpoints
-		assert_ne!(editor.selection, ((0,0), (3,0)));
-		assert_eq!(editor.selection, ((3,0), (6,0)));
+		assert_ne!(editor.selection, ((0, 0), (3, 0)));
+		assert_eq!(editor.selection, ((3, 0), (6, 0)));
 
 		// Check that the content of the highlighted section is correct
-		let selected_string = &editor.content[editor.pos.1][(editor.selection.0).0 as usize..(editor.selection.1).0 as usize];
+		let selected_string = &editor.content[editor.pos.1]
+			[(editor.selection.0).0 as usize..(editor.selection.1).0 as usize];
 		assert_eq!(selected_string, "456");
 		assert_ne!(selected_string, "45");
 	}
 }
+
--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -1,20 +1,19 @@
 pub mod editor {
 
-	use std::{
-		fs::{self, File},
-		iter,
-		path::Path,
-		time::Duration,
-	};
 	use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 	use ratatui::{
 		style::Style,
-		text::{Line, Text},
+		text::{Line, Span, Text},
 		widgets::Paragraph,
+	};
+	use std::{
+		fs::{self, File},
+		path::Path,
+		time::Duration,
 	};
 
 	use config::config::Config;
-	use rayon::iter::{IntoParallelIterator, ParallelExtend, ParallelIterator};
+	use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelExtend, ParallelIterator};
 
 	// Module containing all the functionality of each key. Called in handle_input
 	mod key_functions;
@@ -54,52 +53,53 @@ pub mod editor {
 				// Read in the contents of the file
 				content: Self::parse_file(&filename, config),
 				filename,
-				cursor_pos: (0,0),
+				cursor_pos: (0, 0),
 				pos: (0, 0),
 				start_cursor_set: false,
 				break_loop: false,
-				selection: ((-1,-1), (-1,-1)),
-				width: (0,0),
-				height: (0,0),
-				scroll_offset: (0,0),
+				selection: ((-1, -1), (-1, -1)),
+				width: (0, 0),
+				height: (0, 0),
+				scroll_offset: (0, 0),
 			}
 		}
 
 		// Parse the specified file to a vector of strings (each element representing a line) as a string for the raw data
 		fn parse_file(filename: &String, config: &Config) -> Vec<String> {
 			// Read the file to a string
-			let content = fs::read_to_string(&filename)
-					.expect("Couldn't read file");
+			let content = fs::read_to_string(&filename).expect("Couldn't read file");
 
 			// Vector containing text lines
 			let mut result: Vec<String> = Vec::new();
 			// String of spaces of length tab_width. Used to replace space indentation with tab indentation
-			let tab_spaces = &iter::repeat(" ").take(config.tab_width).collect::<String>();
+			let tab_spaces = " ".repeat(config.tab_width);
 
 			if !content.is_empty() {
 				let line_split: Vec<&str> = content.split('\n').collect();
 				// Split the string into lines
 				let mut lines: Vec<String> = Vec::new();
 				// Convert this split lines into a vector of strings
-				lines.par_extend(line_split
-					.into_par_iter()	// Parallel iterator
-					.map(|line| {   // Operation
-						let temp = String::from(line);
-						temp
-					}));
-				
+				lines.par_extend(
+					line_split
+						.into_par_iter() // Parallel iterator
+						.map(|line| {
+							// Operation
+							String::from(line)
+						}),
+				);
+
 				// Add each line (with space indentation replaced with tabs) to the vector of strings
-				result.par_extend(lines
-					.into_par_iter()
-					.map(|line| {
-						line.replace(tab_spaces, "\t")
-					}));
+				result.par_extend(
+					lines
+						.into_par_iter()
+						.map(|line| line.replace(&tab_spaces, "\t")),
+				);
 				// Return the vector and raw string
 				return result;
 			}
 			// If there is no text in the file being opened, push an empty line to the vector
 			result.push(String::from(""));
-			return result;
+			result
 		}
 
 		// Set the starting position of the editing space cursor
@@ -109,7 +109,7 @@ pub mod editor {
 			self.height = (start.1, start.1 + height);
 
 			// Set the cursor to the beginning of the block
-			self.pos = (1,0);
+			self.pos = (1, 0);
 			self.cursor_pos = (self.width.0 + 1, self.height.0 + 1);
 
 			// Flag that cursor has been initialized
@@ -121,32 +121,52 @@ pub mod editor {
 			// Vector to store the lines
 			let mut lines: Vec<Line> = Vec::new();
 			let content = &self.content;
-			
+
 			// Start tab with a vertical line
 			let mut tab_char = String::from("\u{023D0}");
 			// Iterator to create a string of tab_width - 1 number of spaces
-			tab_char.push_str(&iter::repeat(" ").take(config.tab_width - 1).collect::<String>());
-			
+			tab_char.push_str(&" ".repeat(config.tab_width - 1));
+
 			// Create a vector of Lines (in parallel)
-			lines.par_extend(content.into_par_iter().map(|part| {
-				Line::from(
-					// Display tabs as a series of spaces
-					part.replace(
-						'\t',
-						tab_char.as_str(),
-					)
-				)
+			lines.par_extend(content.into_par_iter().enumerate().map(|(idx, part)| {
+				self.parse_line(config, idx, part, &tab_char)
 			}));
 
-			// Highlight the selected line
-			lines[self.pos.1] = lines[self.pos.1].clone().style(Style::default()
-				.fg(config.theme.editor_highlight_fg_color)
-				.bg(config.theme.editor_highlight_bg_color)
+			// Highlight the line that the cursor is on
+			lines[self.pos.1] = lines[self.pos.1].clone().style(
+				Style::default()
+					.fg(config.theme.line_highlight_fg_color)
+					.bg(config.theme.line_highlight_bg_color),
 			);
 
 			// Return a paragraph from the lines
-			Paragraph::new(Text::from(lines))
-				.scroll(self.scroll_offset)
+			Paragraph::new(Text::from(lines)).scroll(self.scroll_offset)
+		}
+
+		fn parse_line<'a>(&'a self, config: &Config, idx:usize, line: &'a String, tab_char: &String) -> Line {
+			// Split the line into individual words
+			let characters: Vec<char> = line.replace('\t', &tab_char).chars().collect();
+			let mut spans: Vec<Span> = Vec::new();
+			spans.par_extend(characters.into_par_iter().enumerate().map(|(loc, character)| {
+				// If on the first line of selection, only 
+				if idx == self.selection.0.1 as usize || idx == self.selection.1.1 as usize {
+					if loc > self.selection.0.0 as usize && loc < self.selection.1.0 as usize {
+						Span::from(String::from(character))
+							.style(Style::default().bg(config.theme.selection_highlight))
+					} else {
+						Span::from(String::from(character))
+					}
+				// If between the first and last line of the selection, highlight the full line
+				} else if idx > self.selection.0.1 as usize && idx < self.selection.1.1 as usize {
+					Span::from(String::from(character))
+						.style(Style::default().bg(config.theme.selection_highlight))
+				// If not in the selection, don't highlight
+				} else {
+					Span::from(String::from(character))
+				}
+			}));
+
+			Line::from(spans)
 		}
 
 		// Get the key pressed
@@ -155,7 +175,6 @@ pub mod editor {
 			if event::poll(Duration::from_millis(50)).unwrap() {
 				// Read input
 				match event::read().unwrap() {
-
 					// Return the character if only a key (without moodifier key) is pressed
 					Event::Key(KeyEvent {
 						code,
@@ -182,53 +201,53 @@ pub mod editor {
 							}
 							// If delete was pressed, remove the next character
 							KeyCode::Delete => {
-    							key_functions::delete_key(self);
+								key_functions::delete_key(self);
 							}
 							// Left arrow moves cursor left
 							KeyCode::Left => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// Left arrow functionality
 								key_functions::left_arrow(self, config);
 							}
 							// Right arrow moves cursor right
 							KeyCode::Right => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// Right arrow functionality
 								key_functions::right_arrow(self, config);
 							}
 							// Up arrow move cursor up one line
 							KeyCode::Up => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// Up arrow functionality
 								key_functions::up_arrow(self, config);
 							}
 							// Down arrow move cursor down one line
 							KeyCode::Down => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// Down arrow functionality
 								key_functions::down_arrow(self, config);
 							}
 							// Home button moves to beginning of line
 							KeyCode::Home => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// Home key functionality
 								key_functions::home_key(self);
 							}
 							// End button move to end of line
 							KeyCode::End => {
 								// Clear the highlighted selection of text
-								self.selection = ((-1,-1),(-1,-1));
+								self.selection = ((-1, -1), (-1, -1));
 								// End key functionality
 								key_functions::end_key(self, config);
 							}
 							_ => (),
 						}
-					},
+					}
 
 					// Shift modifier key
 					Event::Key(KeyEvent {
@@ -245,7 +264,7 @@ pub mod editor {
 							KeyCode::Right => {
 								key_functions::highlight_right(self, config);
 							}
-							_ => ()
+							_ => (),
 						}
 					}
 
@@ -274,3 +293,4 @@ pub mod editor {
 		}
 	}
 }
+
